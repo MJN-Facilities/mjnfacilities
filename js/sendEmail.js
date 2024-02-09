3f4447675fa2c60875ef1e08b5caf72b14c95169
MHF0 <moh.hesham.f@gmail.com>
2024-02-09 21:55:51 +0300

    update 09022024

--- js/sendEmail.js
+++ js/sendEmail.js
@@ -1,24 +1,22 @@
-const API_URL = "http://localhost:5000";
+const API_URL = ""; // later
 
 const formEmailName = document.querySelector("#form-email-name");
 const formEmailEmail = document.querySelector("#form-email-email");
-const formEmailDatetimePicker = document.querySelector(
-  "#form-email-callBackDatetimepicker"
-);
-const formEmailCallBackDatetimepicker = document.querySelector(
-  "#form-email-callBackTimeDatetimepicker"
-);
-const formEmailPhoneNumber = document.querySelector("#form-email-phoneNumber");
+const formSubject = document.querySelector("#form-subject");
+const formMessage = document.querySelector("#form-message");
 
 const buttonSubmit = document.querySelector("#formEmailsubmitButton");
+const loading = document.querySelector("#formEmailLoadingRe");
+const formEmailSuccessRe = document.querySelector("#formEmailSuccessRe");
 
 buttonSubmit.addEventListener("click", (event) => {
+  loading.classList.remove("d-none");
+  buttonSubmit.classList.add("d-none");
   const data = {
     email: formEmailEmail.value,
     name: formEmailName.value,
-    callBackTime: formEmailCallBackDatetimepicker.value,
-    callBackDate: formEmailDatetimePicker.value,
-    phoneNumber: formEmailPhoneNumber.value,
+    subject: formSubject.value,
+    message: formMessage.value,
   };
 
   event.preventDefault();
@@ -31,9 +29,21 @@ buttonSubmit.addEventListener("click", (event) => {
   })
     .then((res) => res.json())
     .then((data) => {
-      console.log("====================================");
       console.log(data);
-      console.log("====================================");
+      if (data.success) {
+        loading.classList.add("d-none");
+        buttonSubmit.classList.remove("d-none");
+        formEmailSuccessRe.classList.remove("d-none");
+
+        setTimeout(() => {
+          formEmailSuccessRe.classList.add("d-none");
+        }, 2000);
+
+        formEmailName.value = "";
+        formEmailEmail.value = "";
+        formSubject.value = "";
+        formMessage.value = "";
+      }
     })
     .catch((error) => {
       console.error(error);
